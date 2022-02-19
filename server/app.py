09859f45f8ed9a66fe265a23888c253b2d3b6589
Yaroslav <sotnik.edu@ya.ru>
2022-02-19 17:07:28 +0100

    Fix German characters output

--- server/app.py
+++ server/app.py
@@ -15,19 +15,20 @@ db = SQLAlchemy()
 ma = Marshmallow()
 db.init_app(app)
 
+BASE_API_URL = "/api"
+
 
 @app.route("/")
 def index():
     return json.dumps({"name": "kiwiki", "email": "kiwi@kiwi.ki"})
 
 
-@app.route("/addresses/")
+@app.route(BASE_API_URL + "/addresses/", methods=["GET"])
 def get_addresses_list():
 
     addresses = models.Addresses.query.all()
     addresses_schema = schemas.AddressesSchema(many=True)
-
-    return addresses_schema.dumps(addresses)
+    return addresses_schema.dumps(addresses, ensure_ascii=False)
 
 
 app.run()
--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 class DebugConfig(object):
     DEBUG = True
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://kiwi@localhost:5432"
+    SQLALCHEMY_DATABASE_URI = "postgresql://kiwi@localhost:5432?client_encoding=utf8"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
